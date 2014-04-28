1132fb79b51f64c7dc325ca2d9734912e5e34a2f
Lamont Granquist <lamont@scriptkiddie.org>
2014-04-28 11:42:04 -0700

    add Gem.default_bindir to PATH
    this is still /opt/chef/embedded/bin not the user's homedir tho

--- lib/chef-dk/command/exec.rb
+++ lib/chef-dk/command/exec.rb
@@ -25,7 +25,7 @@ module ChefDK
 
       def run(params)
         env = {
-          'PATH' => "#{omnibus_bin_dir}:#{omnibus_embedded_bin_dir}:#{ENV['PATH']}",
+          'PATH' => "#{Gem.default_bindir}:#{omnibus_bin_dir}:#{omnibus_embedded_bin_dir}:#{ENV['PATH']}",
           'GEM_ROOT' => Gem.default_dir.inspect,
           'GEM_HOME' => ENV['GEM_HOME'],
           'GEM_PATH' => Gem.path.join(':'),
