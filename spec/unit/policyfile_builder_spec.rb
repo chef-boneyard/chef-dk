98dd675d7dbb23432071da35ccb60d296e2bdbe5
danielsdeleo <dan@getchef.com>
2014-05-16 16:58:22 -0700

    Update policyfile lock builder spec

--- spec/unit/policyfile_builder_spec.rb
+++ spec/unit/policyfile_builder_spec.rb
@@ -11,37 +11,39 @@ describe "PolicyfileBuilder #FIXME - rename" do
 
   let(:complete_policyfile) do
 
-    Policyfile.build do |p|
+    PolicyfileLock.build do |p|
 
       # Required
       p.name("basic_example")
 
       # Required. Should be fully expanded without roles
-      p.set_runlist_to("recipe[foo]", "recipe[bar]", "recipe[baz::non_default]")
+      p.runlist("recipe[foo]", "recipe[bar]", "recipe[baz::non_default]")
 
-      p.add_cookbook("foo") do |cb|
+      # A cached_cookbook is stored in the cache directory in a subdirectory
+      # given by 'cache_key'. It is assumed to be static (not modified by the
+      # user).
+      p.cached_cookbook("foo") do |cb|
         cb.cache_key("foo-1.0.0")
-        cb.source("https://community.getchef.com/api/cookbooks/foo/1.0.0")
-        cb.use_content_identifier # default (?)
+
+        # Optional attribute that humans can use to understand where a cookbook
+        # came from.
+        cb.origin("https://community.getchef.com/api/cookbooks/foo/1.0.0")
       end
 
-      p.add_cookbook("bar") do |cb|
+      p.cached_cookbook("bar") do |cb|
         cb.cache_key("bar-f59ee7a5bca6a4e606b67f7f856b768d847c39bb")
-        cb.source("git://github.com/opscode-cookbooks/bar.git")
-        cb.use_content_identifier # default (?)
+        cb.origin("git://github.com/opscode-cookbooks/bar.git")
       end
 
-      p.add_cookbook("baz") do |cb|
+      p.local_cookbook("baz") do |cb|
         # for a local source, we assume the cookbook is in development and
         # could be modified, we will check the identifier before uploading
         cb.source("my_cookbooks/baz")
-        cb.use_content_identifier
       end
 
-      p.add_cookbook("dep_of_bar") do |cb|
+      p.cached_cookbook("dep_of_bar") do |cb|
         cb.cache_key("dep_of_bar-1.2.3")
-        cb.source("https://chef-server.example.com/cookbooks/dep_of_bar/1.2.3")
-        cb.use_content_identifier
+        cb.origin("https://chef-server.example.com/cookbooks/dep_of_bar/1.2.3")
       end
     end
 
@@ -61,7 +63,7 @@ describe "PolicyfileBuilder #FIXME - rename" do
           "version" => "1.0.0",
           "identifier" => "168d2102fb11c9617cd8a981166c8adc30a6e915",
           "dotted_decimal_identifier" => id_to_dotted("168d2102fb11c9617cd8a981166c8adc30a6e915"),
-          "source" => "https://community.getchef.com/api/cookbooks/foo/1.0.0",
+          "origin" => "https://community.getchef.com/api/cookbooks/foo/1.0.0",
           "cache_key" => "foo-1.0.0"
         },
 
@@ -69,7 +71,7 @@ describe "PolicyfileBuilder #FIXME - rename" do
           "version" => "2.0.0",
           "identifier" => "feab40e1fca77c7360ccca1481bb8ba5f919ce3a",
           "dotted_decimal_identifier" => id_to_dotted("feab40e1fca77c7360ccca1481bb8ba5f919ce3a"),
-          "source" => "git://github.com/opscode-cookbooks/bar.git",
+          "origin" => "git://github.com/opscode-cookbooks/bar.git",
           "cache_key" => "bar-f59ee7a5bca6a4e606b67f7f856b768d847c39bb"
         },
 
@@ -94,7 +96,7 @@ describe "PolicyfileBuilder #FIXME - rename" do
           "version" => "1.2.3",
           "identifier" => "3d9d097332199fdafc3237c0ec11fcd784c11b4d",
           "dotted_decimal_identifier" => id_to_dotted("3d9d097332199fdafc3237c0ec11fcd784c11b4d"),
-          "source" => "https://chef-server.example.com/cookbooks/dep_of_bar/1.2.3",
+          "origin" => "https://chef-server.example.com/cookbooks/dep_of_bar/1.2.3",
           "cache_key" => "dep_of_bar-1.2.3",
 
         },
@@ -104,5 +106,76 @@ describe "PolicyfileBuilder #FIXME - rename" do
     }
   end
 
+  let(:minimal_policyfile) do
+    PolicyfileLock.build do |p|
+
+      p.name("minimal_policyfile")
+
+      p.run_list("recipe[foo]")
+
+      p.cached_cookbook("foo") do |cb|
+        cb.cache_key("foo-1.0.0")
+      end
+
+    end
+  end
+
+  let(:minimal_policyfile_compiled) do
+    {
+
+      "name" => "minimal_policyfile",
+
+      "run_list" => ["recipe[foo]"],
+
+      "cookbook_locks" => {
+
+        "foo" => {
+          "version" => "1.0.0",
+          "identifier" => "168d2102fb11c9617cd8a981166c8adc30a6e915",
+          "dotted_decimal_identifier" => id_to_dotted("168d2102fb11c9617cd8a981166c8adc30a6e915"),
+          "origin" => "https://community.getchef.com/api/cookbooks/foo/1.0.0",
+          "cache_key" => "foo-1.0.0"
+        },
+      }
+    }
+  end
+
+  let(:custom_identifier_policyfile) do
+
+    PolicyfileLock.build do |p|
+
+      p.name("custom_identifier")
+
+      p.cached_cookbook("foo") do |cb|
+        cb.cache_key("foo-1.0.0")
+
+        # Explicitly set the identifier and dotted decimal identifiers to the
+        # version number (but it could be anything).
+        cb.identifier("1.0.0")
+        cb.dotted_decimal_identifier("1.0.0")
+      end
+    end
+
+  end
+
+  let(:custom_identifier_policyfile_compiled) do
+    {
+
+      "name" => "custom_identifier",
+
+      "run_list" => ["recipe[foo]"],
+
+      "cookbook_locks" => {
+
+        "foo" => {
+          "version" => "1.0.0",
+          "identifier" => "1.0.0",
+          "dotted_decimal_identifier" => "1.0.0",
+          "origin" => "https://community.getchef.com/api/cookbooks/foo/1.0.0",
+          "cache_key" => "foo-1.0.0"
+        },
+      }
+    }
+  end
 
 end
