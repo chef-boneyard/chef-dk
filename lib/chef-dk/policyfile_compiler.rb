a162fdd8b63aef5cd1ab6f8fdfeb5fe9c44356cc
danielsdeleo <dan@getchef.com>
2014-06-03 20:58:50 -0700

    Collect graph modifications and solution constraints from policyfile

--- lib/chef-dk/policyfile_compiler.rb
+++ lib/chef-dk/policyfile_compiler.rb
@@ -15,6 +15,8 @@
 # limitations under the License.
 #
 
+require 'chef/run_list/run_list_item'
+
 module ChefDK
   class PolicyfileCompiler
 
@@ -44,6 +46,11 @@ module ChefDK
       end
     end
 
+    DEFAULT_DEMAND_CONSTRAINT = '>= 0.0.0'.freeze
+
+    # Cookbooks from these sources lock that cookbook to exactly one version
+    SOURCE_TYPES_WITH_FIXED_VERSIONS = [:git, :path].freeze
+
     def self.evaluate(policyfile_string, policyfile_filename)
       compiler = new(policyfile_string, policyfile_filename)
       compiler.evaluate!
@@ -63,6 +70,10 @@ module ChefDK
       @default_source = nil
       @cookbook_source_overrides = {}
     end
+
+    ##
+    # DSL Methods
+    ##
 
     def run_list(*run_list_items)
       run_list_items = run_list_items.flatten
@@ -91,6 +102,62 @@ module ChefDK
       else
         @cookbook_source_overrides[name] = source_opts
       end
+    end
+
+    ##
+    # Compilation Methods
+    ##
+
+    def graph_demands
+      cookbooks_in_run_list.map do |cookbook_name, version_constraint|
+        [ cookbook_name, version_constraint_for(cookbook_name) ]
+      end
+    end
+
+    def artifacts_graph
+      remote_artifacts_graph.merge(local_artifacts_graph)
+    end
+
+    def local_artifacts_graph
+      cookbooks_in_run_list.inject({}) do |local_artifacts, cookbook_name|
+        if cookbook_version_fixed?(cookbook_name)
+          local_artifacts[cookbook_name] = cache_manager.cookbook_dependencies(cookbook_name)
+        end
+        local_artifacts
+      end
+    end
+
+    def remote_artifacts_graph
+      cache_manager.universe_graph
+    end
+
+    def version_constraint_for(cookbook_name)
+      if cookbook_version_fixed?(cookbook_name)
+        version = cache_manager.cookbook_version(cookbook_name)
+        "= #{version}"
+      else
+        DEFAULT_DEMAND_CONSTRAINT
+      end
+    end
+
+    def cookbook_version_fixed?(cookbook_name)
+      if source_options = cookbook_source_overrides[cookbook_name]
+        SOURCE_TYPES_WITH_FIXED_VERSIONS.any? { |type| source_options.key?(type) }
+      else
+        false
+      end
+    end
+
+    def cache_manager
+      raise "TODO"
+    end
+
+    def run_list_graph_demands
+      cookbooks_in_run_list.map {|cookbook_name| [cookbook_name, DEFAULT_DEMAND_CONSTRAINT] }
+    end
+
+    def cookbooks_in_run_list
+      run_list.map {|item_spec| Chef::RunList::RunListItem.new(item_spec).name }
     end
 
     def evaluate!
