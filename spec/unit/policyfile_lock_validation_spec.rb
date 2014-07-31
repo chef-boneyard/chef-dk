2fd983dd18af7f7af86916afc1a1f8df45447777
danielsdeleo <dan@getchef.com>
2014-07-31 09:32:51 -0700

    Add more cases for lockfile validation specs

--- spec/unit/policyfile_lock_validation_spec.rb
+++ spec/unit/policyfile_lock_validation_spec.rb
@@ -16,6 +16,7 @@
 #
 
 require 'spec_helper'
+# TODO: remove if we don't end up using these:
 #require 'shared/setup_git_cookbooks'
 #require 'shared/fixture_cookbook_checksums'
 #require 'chef-dk/policyfile/storage_config'
@@ -23,17 +24,99 @@ require 'chef-dk/policyfile_lock.rb'
 
 describe ChefDK::PolicyfileLock, "validating locked cookbooks" do
 
+  # TODO: most setup here is duplicated from policyfile_lock_install_spec
+
+  let(:cache_path) do
+    File.expand_path("spec/unit/fixtures/cookbook_cache", project_root)
+  end
+
+  let(:policyfile_lock_path) { "/fakepath/Policyfile.lock.json" }
+
+  let(:local_cookbooks_root) do
+    temp_local_path_cookbooks = File.join(tempdir, "local_path_cookbooks")
+    FileUtils.cp_r(File.join(fixtures_path, "local_path_cookbooks"), temp_local_path_cookbooks)
+    temp_local_path_cookbooks
+  end
+
+  let(:name) { "application-server" }
+
+  let(:run_list) { [ 'recipe[erlang::default]', 'recipe[erchef::prereqs]', 'recipe[erchef::app]' ] }
+
+  let(:storage_config) do
+    ChefDK::Policyfile::StorageConfig.new( cache_path: cache_path, relative_paths_root: local_cookbooks_root )
+  end
+
+  let(:lock_generator) do
+    ChefDK::PolicyfileLock.build(storage_config) do |policy|
+
+      policy.name = name
+
+      policy.run_list = run_list
+
+      policy.cached_cookbook("foo") do |c|
+        c.origin = "https://artifact-server.example/foo/1.0.0"
+        c.cache_key = "foo-1.0.0"
+        c.source_options = { artifactserver: "https://artifact-server.example/foo/1.0.0", version: "1.0.0" }
+      end
+
+      policy.local_cookbook("local-cookbook") do |c|
+        c.source = "local-cookbook"
+        c.source_options = { path: "local-cookbook" }
+      end
+
+    end
+  end
+
+  let(:lock_data) do
+    lock_generator.to_lock
+  end
+
+  let(:policyfile_lock) do
+    ChefDK::PolicyfileLock.new(storage_config).build_from_lock_data(lock_data)
+  end
+
   context "when no cookbooks have changed" do
 
-    it "validation succeeds"
+    it "validation succeeds" do
+      expect(policyfile_lock.validate_cookbooks!).to be true
+    end
 
   end
 
   context "when a :path sourced cookbook is missing" do
 
-    it "reports the missing cookbook and fails validation"
+    let(:local_cookbook_path) { File.join(local_cookbooks_root, "local-cookbook") }
+
+    before do
+      FileUtils.rm_rf(local_cookbook_path)
+    end
+
+    it "reports the missing cookbook and fails validation" do
+      skip # code below isn't working at the moment
+      message = "lockfile `Policyfile.lock.json' contains cookbook `local-cookbook' at path `#{local_cookbook_path}', but nothing is there"
+      expect(policyfile_lock.validate_cookbooks!).to raise_error(ChefDK::MissingLockedCookbook, message)
+    end
 
   end
+
+  context "when a :path sourced cookbook has an incorrect name" do
+
+    it "reports the unexpected cookbook and fails validation"
+
+  end
+
+  context "when a :path sourced cookbook has an updated version that violates no dependency constraints" do
+
+    it "updates the version information in the lockfile"
+
+  end
+
+  context "when a :path sourced cookbook has an updated version that violates other dependency constraints" do
+
+    it "reports the dependency conflict and fails validation"
+
+  end
+
 
   context "when a cached cookbook is missing" do
 
@@ -71,6 +154,7 @@ describe ChefDK::PolicyfileLock, "validating locked cookbooks" do
 
   end
 
+  # Cached cookbook is both supermarket and git
   context "when a cached cookbook is modified" do
 
     # This basically means the user modified the cached cookbook. There's no
