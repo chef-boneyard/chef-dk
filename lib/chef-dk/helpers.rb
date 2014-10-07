70a8a4fb23e65cb29b6d3b4e358e84674aab5fe5
Lamont Granquist <lamont@scriptkiddie.org>
2014-10-07 14:27:05 -0700

    fix path joining for windows

--- lib/chef-dk/helpers.rb
+++ lib/chef-dk/helpers.rb
@@ -95,7 +95,7 @@ module ChefDK
         begin
           user_bin_dir = File.expand_path(File.join(Gem.user_dir, 'bin'))
           {
-            'PATH' => "#{omnibus_bin_dir}:#{user_bin_dir}:#{omnibus_embedded_bin_dir}:#{ENV['PATH']}",
+            'PATH' => [ omnibus_bin_dir, user_bin_dir, omnibus_embedded_bin_dir, ENV['PATH'] ].join(File::PATH_SEPARATOR)
             'GEM_ROOT' => Gem.default_dir.inspect,
             'GEM_HOME' => Gem.user_dir,
             'GEM_PATH' => Gem.path.join(':'),
