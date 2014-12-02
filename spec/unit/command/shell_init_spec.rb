6f350468e473d270434587863db3057e7e344237
Jay Mundrawala <jdmundrawala@gmail.com>
2014-12-02 15:24:08 -0800

    Refactor shell_init_spec

--- spec/unit/command/shell_init_spec.rb
+++ spec/unit/command/shell_init_spec.rb
@@ -30,84 +30,90 @@ describe ChefDK::Command::ShellInit do
     end
   end
 
-  before do
-    stub_const("File::PATH_SEPARATOR", ':')
+  shared_context "shell init script" do |shell|
+    let(:user_bin_dir) { File.expand_path(File.join(Gem.user_dir, 'bin')) }
+    let(:expected_path) { [omnibus_bin_dir, user_bin_dir, omnibus_embedded_bin_dir, ENV['PATH']].join(File::PATH_SEPARATOR) }
+    let(:expected_gem_root) { Gem.default_dir.to_s }
+    let(:expected_gem_home) { Gem.user_dir }
+    let(:expected_gem_path) { Gem.path.join(File::PATH_SEPARATOR) }
+
+    context "with no explicit omnibus directory" do
+
+      let(:omnibus_bin_dir) { "/foo/bin" }
+      let(:omnibus_embedded_bin_dir) { "/foo/embedded/bin" }
+      let(:argv) { [shell] }
+
+      before do
+        allow(command_instance).to receive(:omnibus_embedded_bin_dir).and_return(omnibus_embedded_bin_dir)
+        allow(command_instance).to receive(:omnibus_bin_dir).and_return(omnibus_bin_dir)
+      end
+
+      it "emits a script to add ChefDK's ruby to the shell environment" do
+        command_instance.run(argv)
+        expect(stdout_io.string).to eq(expected_environment_commands)
+      end
+    end
+
+    context "with an explicit omnibus directory as an argument" do
+
+      let(:omnibus_root) { File.join(fixtures_path, "eg_omnibus_dir/valid/") }
+      let(:omnibus_bin_dir) { File.join(omnibus_root, "bin") }
+      let(:omnibus_embedded_bin_dir) { File.join(omnibus_root, "embedded/bin") }
+
+      let(:argv) { [shell, "--omnibus-dir", omnibus_root] }
+
+      it "emits a script to add ChefDK's ruby to the shell environment" do
+        command_instance.run(argv)
+        expect(stdout_io.string).to eq(expected_environment_commands)
+      end
+    end
   end
 
-  let(:argv) { ['bash'] }
+  shared_examples "a posix shell script" do |shell|
+    before do
+      stub_const("File::PATH_SEPARATOR", ':')
+    end
 
-  let(:user_bin_dir) { File.expand_path(File.join(Gem.user_dir, 'bin')) }
-
-  let(:expected_path) { [omnibus_bin_dir, user_bin_dir, omnibus_embedded_bin_dir, ENV['PATH']].join(File::PATH_SEPARATOR) }
-
-  let(:expected_gem_root) { Gem.default_dir.to_s }
-
-  let(:expected_gem_home) { Gem.user_dir }
-
-  let(:expected_gem_path) { Gem.path.join(File::PATH_SEPARATOR) }
-
-  let(:expected_environment_commands) do
-    <<-EOH
+    let(:expected_environment_commands) do
+      <<-EOH
 export PATH="#{expected_path}"
 export GEM_ROOT="#{expected_gem_root}"
 export GEM_HOME="#{expected_gem_home}"
 export GEM_PATH="#{expected_gem_path}"
 EOH
+    end
+    include_context "shell init script", shell
   end
 
-  context "with no explicit omnibus directory" do
-
-    let(:omnibus_bin_dir) { "/foo/bin" }
-    let(:omnibus_embedded_bin_dir) { "/foo/embedded/bin" }
-
-    before do
-      allow(command_instance).to receive(:omnibus_embedded_bin_dir).and_return(omnibus_embedded_bin_dir)
-      allow(command_instance).to receive(:omnibus_bin_dir).and_return(omnibus_bin_dir)
+  ['bash', 'sh', 'zsh'].each do |shell|
+    context "for #{shell}" do
+      it_behaves_like "a posix shell script", shell
     end
+  end
 
-    it "emits a script to add ChefDK's ruby to the shell environment" do
-      command_instance.run(argv)
-      expect(stdout_io.string).to eq(expected_environment_commands)
-    end
+  context "when no shell is specified" do
 
-    context "when no shell is specified" do
+    let(:argv) { [] }
 
-      let(:argv) { [] }
-
-      it "exits with an error message" do
-        expect(command_instance.run(argv)).to eq(1)
-        expect(stderr_io.string).to include("Please specify what shell you are using")
-      end
-
-    end
-
-    context "when an unsupported shell is specified" do
-
-      let(:argv) { ['nosuchsh'] }
-
-      it "exits with an error message" do
-        expect(command_instance.run(argv)).to eq(1)
-        expect(stderr_io.string).to include("Shell `nosuchsh' is not currently supported")
-        expect(stderr_io.string).to include("Supported shells are: bash zsh sh powershell posh")
-      end
-
+    it "exits with an error message" do
+      expect(command_instance.run(argv)).to eq(1)
+      expect(stderr_io.string).to include("Please specify what shell you are using")
     end
 
   end
 
-  context "with an explicit omnibus directory as an argument" do
+  context "when an unsupported shell is specified" do
 
-    let(:omnibus_root) { File.join(fixtures_path, "eg_omnibus_dir/valid/") }
-    let(:omnibus_bin_dir) { File.join(omnibus_root, "bin") }
-    let(:omnibus_embedded_bin_dir) { File.join(omnibus_root, "embedded/bin") }
+    let(:argv) { ['nosuchsh'] }
 
-    let(:argv) { ["bash", "--omnibus-dir", omnibus_root] }
-
-    it "emits a script to add ChefDK's ruby to the shell environment" do
-      command_instance.run(argv)
-      expect(stdout_io.string).to eq(expected_environment_commands)
+    it "exits with an error message" do
+      expect(command_instance.run(argv)).to eq(1)
+      expect(stderr_io.string).to include("Shell `nosuchsh' is not currently supported")
+      expect(stderr_io.string).to include("Supported shells are: bash zsh sh powershell posh")
     end
+
   end
+
 
 end
 
