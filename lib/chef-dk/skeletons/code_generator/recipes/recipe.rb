5e250215bbed6f19036cd9f5bda3e0977660fd59
Charles Johnson <charles@opscode.com>
2014-12-09 17:56:06 -0800

    Add Chefspec support to recipe generator
    - Update recipe generator to create Chefspec files
    - Updated recipe_spec.rb to ensure that Chefspec files are created

--- lib/chef-dk/skeletons/code_generator/recipes/recipe.rb
+++ lib/chef-dk/skeletons/code_generator/recipes/recipe.rb
@@ -2,8 +2,29 @@
 context = ChefDK::Generator.context
 cookbook_dir = File.join(context.cookbook_root, context.cookbook_name)
 recipe_path = File.join(cookbook_dir, "recipes", "#{context.new_file_basename}.rb")
+spec_helper_path = File.join(cookbook_dir, "spec", "spec_helper.rb")
+spec_path = File.join(cookbook_dir, "spec", "unit", "recipes", "#{context.new_file_basename}_spec.rb")
 
+# Chefspec
+directory "#{cookbook_dir}/spec/unit/recipes" do
+  recursive true
+end
+
+cookbook_file spec_helper_path do
+  action :create_if_missing
+end
+
+template spec_path do
+  source "recipe_spec.rb.erb"
+  helpers(ChefDK::Generator::TemplateHelper)
+  variables(
+    :recipe_name => context.new_file_basename)
+end
+
+# Recipe
 template recipe_path do
   source "recipe.rb.erb"
   helpers(ChefDK::Generator::TemplateHelper)
+  variables(
+    :recipe_name => context.new_file_basename)
 end
