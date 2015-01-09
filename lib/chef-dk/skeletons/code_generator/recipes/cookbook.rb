Marchex/marchex-chef-generator

Added serverspec to cookbook & app generators
chef generate will now automatically create a serverspec test for the default suite when it generates a .kitchen.yml.
- Updated app_spec.rb to check for serverspec suite
- Updated cookbook_spec.rb to check for serverspec suite
- Updated app.rb recipe to generate serverspec test for default suite
- Updated cookbook.rb recipe to generate serverspec test for default suite
- Added file for serverspec_spec_helper.rb
- Added template for serverspec_default_spec.rb

recipes/cookbook.rb
@@ -32,6 +32,21 @@ template "#{cookbook_dir}/.kitchen.yml" do
   action :create_if_missing
 end
 
+directory "#{cookbook_dir}/test/integration/default/serverspec" do
+  recursive true
+end
+
+cookbook_file "#{cookbook_dir}/test/integration/default/serverspec/spec_helper.rb" do
+  source 'serverspec_spec_helper.rb'
+  action :create_if_missing
+end
+
+template "#{cookbook_dir}/test/integration/default/serverspec/default_spec.rb" do
+  source 'serverspec_default_spec.rb.erb'
+  helpers(ChefDK::Generator::TemplateHelper)
+  action :create_if_missing
+end
+
 # Recipes
 
 directory "#{cookbook_dir}/recipes"

chef-boneyard/chef-dk

lib/chef-dk/skeletons/code_generator/recipes/cookbook.rb
@@ -25,15 +25,29 @@ cookbook_file "#{cookbook_dir}/Berksfile" do
   action :create_if_missing
 end
 
-# TK
+# TK and Serverspec
 template "#{cookbook_dir}/.kitchen.yml" do
   source 'kitchen.yml.erb'
   helpers(ChefDK::Generator::TemplateHelper)
   action :create_if_missing
 end
 
-# Chefspec
+directory "#{app_dir}/test/integration/default/serverspec" do
+  recursive true
+end
 
+cookbook_file "#{app_dir}/test/integration/default/serverspec/spec_helper.rb" do
+  source 'serverspec_spec_helper.rb'
+  action :create_if_missing
+end
+
+template "#{app_dir}/test/integration/default/serverspec/default_spec.rb" do
+  source 'serverspec_default_spec.rb.erb'
+  helpers(ChefDK::Generator::TemplateHelper)
+  action :create_if_missing
+end
+
+# Chefspec
 directory "#{cookbook_dir}/spec/unit/recipes" do
   recursive true
 end
@@ -46,20 +60,15 @@ template "#{cookbook_dir}/spec/unit/recipes/default_spec.rb" do
   source "recipe_spec.rb.erb"
   helpers(ChefDK::Generator::TemplateHelper)
   action :create_if_missing
-  variables(
-  :recipe_name => 'default')
 end
 
 # Recipes
-
 directory "#{cookbook_dir}/recipes"
 
 template "#{cookbook_dir}/recipes/default.rb" do
   source "recipe.rb.erb"
   helpers(ChefDK::Generator::TemplateHelper)
   action :create_if_missing
-  variables(
-  :recipe_name => 'default')
 end
 
 # git