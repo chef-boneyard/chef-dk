1ec06e305b62d41099cfbeab008591b7a185f88c
danielsdeleo <dan@getchef.com>
2015-04-09 12:31:01 -0700

    Add service object for attribute-only update of policy locks

--- lib/chef-dk/policyfile_services/update_attributes.rb
+++ lib/chef-dk/policyfile_services/update_attributes.rb
@@ -1,5 +1,5 @@
 #
-# Copyright:: Copyright (c) 2014 Chef Software Inc.
+# Copyright:: Copyright (c) 2015 Chef Software Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,50 +15,62 @@
 # limitations under the License.
 #
 
-require 'ffi_yajl'
-
 require 'chef-dk/helpers'
+require 'chef-dk/policyfile/storage_config'
 require 'chef-dk/service_exceptions'
 require 'chef-dk/policyfile_compiler'
-require 'chef-dk/policyfile/storage_config'
-require 'chef-dk/policyfile_lock'
 
 module ChefDK
   module PolicyfileServices
 
-    class Install
+    class UpdateAttributes
 
       include Policyfile::StorageConfigDelegation
       include ChefDK::Helpers
 
       attr_reader :ui
       attr_reader :storage_config
-      attr_reader :overwrite
 
-      def initialize(policyfile: nil, ui: nil, root_dir: nil, overwrite: false)
+      def initialize(policyfile: nil, ui: nil, root_dir: nil)
         @ui = ui
-        @overwrite = overwrite
 
         policyfile_rel_path = policyfile || "Policyfile.rb"
         policyfile_full_path = File.expand_path(policyfile_rel_path, root_dir)
         @storage_config = Policyfile::StorageConfig.new.use_policyfile(policyfile_full_path)
-
-        @policyfile_content = nil
-        @policyfile_compiler = nil
+        @updated = false
       end
 
       def run
         unless File.exist?(policyfile_expanded_path)
-          # TODO: suggest next step. Add a generator/init command? Specify path to Policyfile.rb?
-          # See card CC-232
           raise PolicyfileNotFound, "Policyfile not found at path #{policyfile_expanded_path}"
         end
-
-        if installing_from_lock?
-          install_from_lock
-        else
-          generate_lock_and_install
+        unless File.exist?(policyfile_lock_expanded_path)
+          raise LockfileNotFound, "Policyfile lock not found at path #{policyfile_lock_expanded_path}"
         end
+
+        if policyfile_compiler.default_attributes != policyfile_lock.default_attributes
+          policyfile_lock.default_attributes = policyfile_compiler.default_attributes
+          @updated = true
+        end
+
+        if policyfile_compiler.override_attributes != policyfile_lock.override_attributes
+          policyfile_lock.override_attributes = policyfile_compiler.override_attributes
+          @updated = true
+        end
+
+        if updated_lock?
+          with_file(policyfile_lock_expanded_path) do |f|
+            f.print(FFI_Yajl::Encoder.encode(policyfile_lock.to_lock, pretty: true ))
+          end
+          ui.msg("Updated attributes in #{policyfile_lock_expanded_path}")
+        else
+          ui.msg("Attributes already up to date")
+        end
+
+      end
+
+      def updated_lock?
+        @updated
       end
 
       def policyfile_content
@@ -69,57 +81,17 @@ module ChefDK
         @policyfile_compiler ||= ChefDK::PolicyfileCompiler.evaluate(policyfile_content, policyfile_expanded_path, ui: ui)
       end
 
-      def expanded_run_list
-        policyfile_compiler.expanded_run_list.to_s
-      end
-
       def policyfile_lock_content
-        @policyfile_lock_content ||= IO.read(policyfile_lock_expanded_path) if File.exist?(policyfile_lock_expanded_path)
+        @policyfile_lock_content ||= IO.read(policyfile_lock_expanded_path)
       end
 
       def policyfile_lock
-        return nil if policyfile_lock_content.nil?
         @policyfile_lock ||= begin
           lock_data = FFI_Yajl::Parser.new.parse(policyfile_lock_content)
           PolicyfileLock.new(storage_config, ui: ui).build_from_lock_data(lock_data)
         end
       end
-
-      def generate_lock_and_install
-        policyfile_compiler.error!
-
-        ui.msg "Building policy #{policyfile_compiler.name}"
-        ui.msg "Expanded run list: " + expanded_run_list + "\n"
-
-        ui.msg "Caching Cookbooks..."
-
-        policyfile_compiler.install
-
-        lock_data = policyfile_compiler.lock.to_lock
-
-        with_file(policyfile_lock_expanded_path) do |f|
-          f.print(FFI_Yajl::Encoder.encode(lock_data, pretty: true ))
-        end
-
-        ui.msg ""
-
-        ui.msg "Lockfile written to #{policyfile_lock_expanded_path}"
-      rescue => error
-        raise PolicyfileInstallError.new("Failed to generate Policyfile.lock", error)
-      end
-
-      def install_from_lock
-        ui.msg "Installing cookbooks from lock"
-
-        policyfile_lock.install_cookbooks
-      rescue => error
-        raise PolicyfileInstallError.new("Failed to install cookbooks from lockfile", error)
-      end
-
-      def installing_from_lock?
-        !@overwrite && File.exist?(policyfile_lock_expanded_path)
-      end
-
     end
   end
 end
+
