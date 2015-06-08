0445de1e18c7fbf716ce06e9f2e2944d16e94fc4
danielsdeleo <dan@getchef.com>
2015-06-08 14:43:02 -0700

    Move policyfile info fetcher to its own file

--- lib/chef-dk/command/show_policy.rb
+++ lib/chef-dk/command/show_policy.rb
@@ -18,161 +18,10 @@
 require 'chef-dk/command/base'
 require 'chef-dk/ui'
 require 'chef-dk/configurable'
-require 'set'
+require 'chef-dk/policyfile_services/info_fetcher'
 
 module ChefDK
   module Command
-
-    class RevIDLockDataMap
-
-      attr_reader :policy_name
-      attr_reader :lock_info_by_rev_id
-
-      def initialize(policy_name, lock_info_by_rev_id)
-        @policy_name = policy_name
-        @lock_info_by_rev_id = lock_info_by_rev_id
-      end
-
-
-      def cb_info_for(rev_id, cookbook_name)
-        lock = lock_info_by_rev_id[rev_id]
-        cookbook_lock = lock["cookbook_locks"][cookbook_name]
-
-        if cookbook_lock
-          [cookbook_lock["version"], cookbook_lock["identifier"] ]
-        else
-          nil
-        end
-      end
-
-      def cbs_with_differing_ids
-        cbs_with_differing_ids = Set.new
-        all_cookbook_names.each do |cookbook_name|
-          all_identifiers = lock_info_by_rev_id.inject(Set.new) do |id_set, (_rev_id, rev_info)|
-            cookbook_lock = rev_info["cookbook_locks"][cookbook_name]
-            identifier = cookbook_lock && cookbook_lock["identifier"]
-            id_set << identifier
-          end
-          cbs_with_differing_ids << cookbook_name if all_identifiers.size > 1
-        end
-        cbs_with_differing_ids
-      end
-
-      def all_cookbook_names
-        lock_info_by_rev_id.inject(Set.new) do |cb_set, (_rev_id, rev_info)|
-          cb_set.merge(rev_info["cookbook_locks"].keys)
-        end
-      end
-    end
-
-    class PolicyGroupRevIDMap
-
-      attr_reader :policy_name
-      attr_reader :revision_ids_by_group
-
-      def initialize(policy_name, revision_ids_by_group)
-        @policy_name = policy_name
-        @revision_ids_by_group = revision_ids_by_group
-      end
-
-      def unique_revision_ids
-        revision_ids_by_group.values.uniq
-      end
-
-      def policy_group_names
-        revision_ids_by_group.keys
-      end
-
-      def max_group_name_length
-        policy_group_names.map(&:size).max
-      end
-
-      def format_revision_ids
-        revision_ids_by_group.inject({}) do |map, (group_name, rev_id)|
-          map[group_name] = yield rev_id
-          map
-        end
-      end
-
-      def empty?
-        policy_group_names.empty?
-      end
-
-      def each
-        revision_ids_by_group.each do |group_name, rev_id|
-          yield group_name, rev_id
-        end
-      end
-    end
-
-    class PolicyInfoFetcher
-
-      attr_accessor :policies_by_name
-
-      # A Hash with the following format:
-      #   "dev" => {
-      #     "appserver" => "1111111111111111111111111111111111111111111111111111111111111111",
-      #     "load-balancer" => "5555555555555555555555555555555555555555555555555555555555555555",
-      #     "db" => "9999999999999999999999999999999999999999999999999999999999999999"
-      #   }
-      attr_accessor :policies_by_group
-
-      attr_accessor :policy_lock_content
-
-      def initialize
-        @policies_by_name = {}
-        @policies_by_group = {}
-        @policy_lock_content = {}
-        @active_revisions = nil
-      end
-
-      def set!(policies_by_name, policies_by_group)
-        @policies_by_name = policies_by_name
-        @policies_by_group = policies_by_group
-        @active_revisions = nil
-      end
-
-      def revision_info_for(policy_name, _revision_id_list)
-        RevIDLockDataMap.new(policy_name, policy_lock_content[policy_name])
-      end
-
-      def revision_ids_by_group_for_each_policy
-        policies_by_name.each do |policy_name, _policies|
-          rev_id_by_group = revision_ids_by_group_for(policy_name)
-          yield policy_name, rev_id_by_group
-        end
-      end
-
-      def revision_ids_by_group_for(policy_name)
-        map = policies_by_group.inject({}) do |rev_id_map, (group_name, rev_id_map_for_group)|
-          rev_id_map[group_name] = rev_id_map_for_group[policy_name]
-          rev_id_map
-        end
-        PolicyGroupRevIDMap.new(policy_name, map)
-      end
-
-      def orphaned_revisions(policy_name)
-        orphans = []
-        policies_by_name[policy_name].each do |rev_id, _data|
-          orphans << rev_id unless active_revisions.include?(rev_id)
-        end
-        orphans
-      end
-
-      def active_revisions
-        @active_revisions ||= policies_by_group.inject(Set.new) do |set, (_group, policy_name_rev_id_map)|
-          policy_name_rev_id_map.each do |policy_name, rev_id|
-            set << rev_id
-          end
-          set
-        end
-      end
-
-      def empty?
-        policies_by_name.empty? && policies_by_group.empty?
-      end
-
-    end
 
     class ReportPrinter
 
@@ -371,7 +220,7 @@ BANNER
       end
 
       def policy_info_fetcher
-        @policy_info_fetcher ||= PolicyInfoFetcher.new
+        @policy_info_fetcher ||= PolicyfileServices::InfoFetcher.new
       end
 
       def debug?
---
+++ spec/unit/policyfile_services/info_fetcher_spec.rb
@@ -0,0 +1,305 @@
+#
+# Copyright:: Copyright (c) 2015 Chef Software Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'spec_helper'
+require 'chef-dk/policyfile_services/info_fetcher'
+
+describe ChefDK::PolicyfileServices::InfoFetcher do
+
+  def api_url(org_specific_path)
+    "https://chef.example/organizations/myorg/#{org_specific_path}"
+  end
+
+
+  let(:config) do
+    double("Chef::Config",
+           chef_server_url: "https://localhost:10443",
+           client_key: "/path/to/client/key.pem",
+           node_name: "deuce")
+  end
+
+  let(:http_client) { instance_double(ChefDK::AuthenticatedHTTP) }
+
+  subject(:info_fetcher) do
+    described_class.new(config: config)
+  end
+
+  it "configures an HTTP client" do
+    expect(ChefDK::AuthenticatedHTTP).to receive(:new).with("https://localhost:10443",
+                                                       signing_key_filename: "/path/to/client/key.pem",
+                                                       client_name: "deuce")
+    info_fetcher.http_client
+  end
+
+  describe "when an error occurs in a GET request" do
+
+    # TODO: make this reusable
+    let(:response) do
+      Net::HTTPResponse.send(:response_class, "500").new("1.0", "500", "Internal Server Error").tap do |r|
+        r.instance_variable_set(:@body, "oops")
+      end
+    end
+
+    let(:http_exception) do
+      begin
+        response.error!
+      rescue => e
+        e
+      end
+    end
+
+    let(:policies_url) { "/policies" }
+
+    let(:policy_groups_url) { "/policy_groups" }
+
+    before do
+      allow(info_fetcher).to receive(:http_client).and_return(http_client)
+    end
+
+    describe "when fetching policy revisions by policy group" do
+
+      before do
+        allow(http_client).to receive(:get).with("policies").and_return({})
+        expect(http_client).to receive(:get).with("policy_groups").and_raise(http_exception)
+      end
+
+      it "raises an error" do
+        expect { info_fetcher.policies_by_group }.to raise_error(ChefDK::PolicyfileListError)
+      end
+    end
+
+    describe "when fetching policy revisions by policy name" do
+
+      before do
+        allow(http_client).to receive(:get).with("policy_groups").and_return({})
+        expect(http_client).to receive(:get).with("policies").and_raise(http_exception)
+      end
+
+      it "raises an error" do
+        expect { info_fetcher.policies_by_name }.to raise_error(ChefDK::PolicyfileListError)
+      end
+    end
+  end
+
+  context "when the data is fetched successfully from the server" do
+
+    before do
+      allow(info_fetcher).to receive(:http_client).and_return(http_client)
+
+      allow(http_client).to receive(:get).with("policy_groups").and_return(policy_group_list_data)
+      allow(http_client).to receive(:get).with("policies").and_return(policy_list_data)
+    end
+
+    context "when the server has no policies or groups" do
+
+      let(:policy_group_list_data) { {} }
+      let(:policy_list_data) { {} }
+
+      it "gives a Hash of policy revisions by policy name" do
+        expect(info_fetcher.policies_by_name).to eq({})
+      end
+
+      it "gives a Hash of policy revisions by policy group" do
+        expect(info_fetcher.policies_by_group).to eq({})
+      end
+
+      it "is empty" do
+        expect(info_fetcher).to be_empty
+      end
+
+      it "has no active revisions" do
+        expect(info_fetcher.active_revisions).to be_empty
+      end
+    end
+
+    context "when the server has policies and groups" do
+      ##
+      # Example API response data copied from oc-chef-pedant:
+
+      let(:policy_list_data) do
+        {
+          "appserver" => {
+            "uri" => api_url("policies/appserver"),
+            "revisions" => {
+              "1111111111111111111111111111111111111111" => {},
+              "2222222222222222222222222222222222222222" => {},
+              "3333333333333333333333333333333333333333" => {},
+              "4444444444444444444444444444444444444444" => {}
+            }
+          },
+          "db" => {
+            "uri" => api_url("policies/db"),
+            "revisions" => {
+              "6666666666666666666666666666666666666666" => {},
+              "7777777777777777777777777777777777777777" => {},
+              "8888888888888888888888888888888888888888" => {},
+              "9999999999999999999999999999999999999999" => {}
+            }
+          },
+          "cache" => {
+            "uri" => api_url("policies/cache"),
+            "revisions" => {
+              "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" => {},
+              "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb" => {}
+            }
+          }
+        }
+      end
+
+      let(:dev_group_data) do
+        {
+          "uri" => api_url("policy_groups/dev"),
+          "policies" => {
+            "db" => { "revision_id" => "6666666666666666666666666666666666666666" },
+            "appserver" => { "revision_id" => "1111111111111111111111111111111111111111" },
+            "cache" => { "revision_id" => "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" }
+          }
+        }
+      end
+
+      let(:test_group_data) do
+        {
+          "uri" => api_url("policy_groups/test"),
+          "policies" => {
+            "db" => { "revision_id" => "7777777777777777777777777777777777777777" },
+            "appserver" => { "revision_id" => "2222222222222222222222222222222222222222" },
+            "cache" => { "revision_id" => "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb" }
+          }
+        }
+      end
+
+      let(:prod_group_data) do
+        {
+          "uri" => api_url("policy_groups/prod"),
+          "policies" => {
+            "db" => { "revision_id" => "8888888888888888888888888888888888888888" },
+            "appserver" => { "revision_id" => "3333333333333333333333333333333333333333" }
+          }
+        }
+      end
+
+      let(:policy_group_list_data) do
+        {
+          "dev" => dev_group_data,
+          "test" => test_group_data,
+          "prod" => prod_group_data
+        }
+      end
+
+      let(:expected_policy_list) do
+        {
+          "appserver" => {
+            "1111111111111111111111111111111111111111" => {},
+            "2222222222222222222222222222222222222222" => {},
+            "3333333333333333333333333333333333333333" => {},
+            "4444444444444444444444444444444444444444" => {}
+          },
+          "db" => {
+            "6666666666666666666666666666666666666666" => {},
+            "7777777777777777777777777777777777777777" => {},
+            "8888888888888888888888888888888888888888" => {},
+            "9999999999999999999999999999999999999999" => {}
+          },
+          "cache" => {
+            "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" => {},
+            "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb" => {}
+          }
+        }
+      end
+
+      let(:expected_policy_group_list) do
+        {
+          "dev" => {
+            "db" => "6666666666666666666666666666666666666666",
+            "appserver" => "1111111111111111111111111111111111111111",
+            "cache" => "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+          },
+          "test" => {
+            "db" => "7777777777777777777777777777777777777777",
+            "appserver" => "2222222222222222222222222222222222222222",
+            "cache" => "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
+          },
+          "prod" => {
+            "db" =>"8888888888888888888888888888888888888888",
+            "appserver" => "3333333333333333333333333333333333333333"
+          }
+        }
+      end
+
+
+      it "gives a Hash of policy revisions by policy name" do
+        expect(info_fetcher.policies_by_name).to eq(expected_policy_list)
+      end
+
+      it "gives a Hash of policy revisions by policy group" do
+        expect(info_fetcher.policies_by_group).to eq(expected_policy_group_list)
+      end
+
+      it "is not empty" do
+        expect(info_fetcher).to_not be_empty
+      end
+
+      it "lists active revisions" do
+        expected_active_revisions = expected_policy_group_list.values.map(&:values).flatten
+
+        expected_active_revisions_set = Set.new(expected_active_revisions)
+        expect(info_fetcher.active_revisions).to eq(expected_active_revisions_set)
+      end
+
+      it "lists orphaned revisions for a given policy" do
+        expect(info_fetcher.orphaned_revisions("db")).to eq(%w[ 9999999999999999999999999999999999999999 ])
+        expect(info_fetcher.orphaned_revisions("appserver")).to eq(%w[ 4444444444444444444444444444444444444444 ])
+        expect(info_fetcher.orphaned_revisions("cache")).to eq([])
+      end
+
+      it "yields revision ids by group" do
+        map = {}
+
+        info_fetcher.revision_ids_by_group_for_each_policy do |policy_name, rev_id_by_group|
+          map[policy_name] = rev_id_by_group
+        end
+
+        appserver_rev_ids = {
+          "dev" => "1111111111111111111111111111111111111111",
+          "test" => "2222222222222222222222222222222222222222",
+          "prod" => "3333333333333333333333333333333333333333"
+        }
+
+        expect(map["appserver"].revision_ids_by_group).to eq(appserver_rev_ids)
+
+        db_rev_ids = {
+          "dev" => "6666666666666666666666666666666666666666",
+          "test" => "7777777777777777777777777777777777777777",
+          "prod" => "8888888888888888888888888888888888888888"
+        }
+
+        expect(map["db"].revision_ids_by_group).to eq(db_rev_ids)
+
+        cache_rev_ids = {
+          "dev" => "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+          "test" => "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+          "prod" => nil
+        }
+        expect(map["cache"].revision_ids_by_group).to eq(cache_rev_ids)
+      end
+
+    end
+
+  end
+
+end
+
