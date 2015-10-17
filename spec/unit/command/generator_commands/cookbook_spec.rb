6a565e9e1777deb7112cc0404a58acfb47136633
Clinton Wolfe <clinton@omniti.com>
2015-10-16 17:39:16 -0700

    Fix quotes in expected output for Policyfile

--- spec/unit/command/generator_commands/cookbook_spec.rb
+++ spec/unit/command/generator_commands/cookbook_spec.rb
@@ -165,16 +165,16 @@ describe ChefDK::Command::GeneratorCommands::Cookbook do
 # https://github.com/opscode/chef-dk/blob/master/POLICYFILE_README.md
 
 # A name that describes what the system you're building with Chef does.
-name "new_cookbook"
+name 'new_cookbook'
 
 # Where to find external cookbooks:
 default_source :supermarket
 
 # run_list: chef-client will run these recipes in the order specified.
-run_list "new_cookbook::default"
+run_list 'new_cookbook::default'
 
 # Specify a custom source for a single cookbook:
-cookbook "new_cookbook", path: "."
+cookbook 'new_cookbook', path: '.'
 POLICYFILE_RB
       end
 
