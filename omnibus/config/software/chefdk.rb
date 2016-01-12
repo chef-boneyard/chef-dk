848a88accf3c92f64c4ebdb191e460fd02c062b7
Kartik Null Cating-Subramanian <ksubramanian@chef.io>
2016-01-12 15:38:13 -0500

    Don't sync vendor/bundler directory under omnibus

--- omnibus/config/software/chefdk.rb
+++ omnibus/config/software/chefdk.rb
@@ -20,13 +20,19 @@ default_version "local_source"
 # For the specific super-special version "local_source", build the source from
 # the local git checkout. This is what you'd want to occur by default if you
 # just ran omnibus build locally.
-version("local_source") { source path: "#{project.files_path}/../.." }
-#TODO: Check if I need to exclude any current omnibus generated files.
-# options: {:exclude => [some paths]}
+version("local_source") do
+  source path: "#{project.files_path}/../.."
+         # Since we are using the local repo, we try to not copy any files
+         # that are generated in the process of bundle installing omnibus.
+         # If the install steps are well-behaved, this should not matter
+         # since we only perform bundle and gem installs from the
+         # omnibus cache source directory, but we do this regardless
+         # to maintain consistency between what a local build sees and
+         # what a github based build will see.
+         options: { exclude: [ "omnibus/vendor" ] }
+end
 
 # For any version other than "local_source", fetch from github.
-# This is the behavior the transitive omnibus software deps such as chef-dk
-# expect.
 if version != "local_source"
   source git: "git://github.com/chef/chef-dk.git"
 end
