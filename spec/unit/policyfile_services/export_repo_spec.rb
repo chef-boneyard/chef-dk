9bcd86cfb897b77de1ad431a06ed2a5dcf1cb8d9
danielsdeleo <dan@chef.io>
2016-01-28 16:42:37 -0800

    Add version check in exported repo's config file
    Exported repos will need Chef 12.7 features (policyfile native mode
    APIs w/ ChefFS), so add a check for them to the config file.

--- spec/unit/policyfile_services/export_repo_spec.rb
+++ spec/unit/policyfile_services/export_repo_spec.rb
@@ -282,6 +282,19 @@ policy_group 'local'
 use_policyfile true
 policy_document_native_api true
 
+# In order to use this repo, you need a version of Chef Client and Chef Zero
+# that supports policyfile "native mode" APIs:
+current_version = Gem::Version.new(Chef::VERSION)
+unless Gem::Requirement.new(">= 12.7").satisfied_by?(current_version)
+  puts("!" * 80)
+  puts(<<-MESSAGE)
+This Chef Repo requires features introduced in Chef 12.7, but you are using
+Chef 12.7.0. Please upgrade to Chef 12.7 or later.
+MESSAGE
+  puts("!" * 80)
+  exit!(1)
+end
+
 CONFIG
             config_path = File.join(export_dir, "client.rb")
             expect(File).to exist(config_path)
