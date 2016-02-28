02de86a1c43c17b04fd53d494c471eb18648d2d3
sawanoboly <sawanoboriyu@higanworks.com>
2016-02-28 17:23:15 +0900

    Create parent directory before generate policyfile

--- lib/chef-dk/skeletons/code_generator/recipes/policyfile.rb
+++ lib/chef-dk/skeletons/code_generator/recipes/policyfile.rb
@@ -2,6 +2,11 @@
 context = ChefDK::Generator.context
 policyfile_path = File.join(context.policyfile_dir, "#{context.new_file_basename}.rb")
 
+directory File.dirname(policyfile_path) do
+  action :create
+  recursive true
+end
+
 template policyfile_path do
   source "Policyfile.rb.erb"
   helpers(ChefDK::Generator::TemplateHelper)
