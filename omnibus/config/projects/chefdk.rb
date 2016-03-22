7bbd4d79274c98f9baf77c16f53e7b816df43e09
danielsdeleo <dan@chef.io>
2016-03-22 14:17:23 -0700

    Pin omnibus libraries to latest versions

--- omnibus/config/projects/chefdk.rb
+++ omnibus/config/projects/chefdk.rb
@@ -78,6 +78,30 @@ override :'chef-provisioning-azure', version: "v0.5.0"
 override :'chef-provisioning-fog', version: "v0.16.0"
 override :'chef-provisioning-vagrant', version: "v0.11.0"
 
+# Lower level library pins
+override :xproto,             version: "7.0.28"
+override :"util-macros",      version: "1.19.0"
+override :makedepend,         version: "1.0.5"
+
+## We are currently on the latest of these:
+#override :"ncurses",          version: "5.9"
+#override :"zlib",             version: "1.2.8"
+#override :"pkg-config-lite",  version: "0.28-1"
+#override :"libffi",           version: "3.2.1"
+#override :"libyaml",          version: "0.1.6"
+#override :"libiconv",         version: "1.14"
+#override :"liblzma",          version: "5.2.2"
+#override :"libxml2",          version: "2.9.3"
+#override :"libxslt",          version: "1.1.28"
+
+## according to comment in omnibus-sw, latest versions don't work on solaris
+# https://github.com/chef/omnibus-software/blob/aefb7e79d29ca746c3f843673ef5e317fa3cba54/config/software/libtool.rb#L23
+#override :"libtool"
+
+## These can float as they are frequently updated in a way that works for us
+#override :"cacerts",                             # probably best to float?
+#override :"openssl"                              # leave this?
+
 dependency "preparation"
 dependency "rb-readline"
 dependency "chefdk"
