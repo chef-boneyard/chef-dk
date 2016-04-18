61094ed50c9c20b73bf270003a7dd438eb40157d
John Keiser <john@johnkeiser.com>
2016-04-18 13:53:28 -0700

    Use just one Gemfile across Windows and Linux Copy all the improvements made in chef

--- tasks/bundle.rb
+++ tasks/bundle.rb
@@ -17,10 +17,11 @@
 
 require_relative "bundle_util"
 require_relative "../version_policy"
+require "fileutils"
 
 desc "Tasks to work with the main Gemfile and Gemfile.<platform>"
 namespace :bundle do
-  desc "Update Gemfile.lock and all Gemfile.<platform>.locks (or one or more gems via bundle:update gem1 gem2 ...). NOTE: use rake dependencies:update_current_chef to update the version of chef first, if you want it updated as well)."
+  desc "Update Gemfile.lock and all Gemfile.<platform>.locks (or one or more gems via bundle:update gem1 gem2 ...)."
   task :update, [:args] do |t, rake_args|
     extend BundleUtil
     args = rake_args[:args] || ""
@@ -31,11 +32,7 @@ namespace :bundle do
       puts "-------------------------------------------------------------------"
       bundle "install #{args}", delete_gemfile_lock: true
       platforms.each do |platform|
-        puts ""
-        puts "-------------------------------------------------------------------"
-        puts "Updating Gemfile.#{platform}.lock ..."
-        puts "-------------------------------------------------------------------"
-        bundle "lock", gemfile: "Gemfile.#{platform}", platform: platform, delete_gemfile_lock: true
+        bundle "lock", platform: platform
       end
     end
   end
@@ -51,18 +48,14 @@ namespace :bundle do
       puts "-------------------------------------------------------------------"
       bundle "install #{args}"
       platforms.each do |platform|
-        puts ""
-        puts "-------------------------------------------------------------------"
-        puts "Updating Gemfile.#{platform}.lock (conservatively) ..."
-        puts "-------------------------------------------------------------------"
-        bundle "lock", gemfile: "Gemfile.#{platform}", platform: platform
+        bundle "lock", platform: platform
       end
     end
   end
 end
 
 desc "Run bundle with arbitrary args against the given platform; e.g. rake bundle[show]. No platform to run against the main bundle; bundle[show,windows] to run the windows one; bundle[show,*] to run against all non-default platforms."
-task :bundle, [:args,:platform] do |t, rake_args|
+task :bundle, [:args, :platform] do |t, rake_args|
   extend BundleUtil
   args = rake_args[:args] || ""
   platform = rake_args[:platform]
