0140c77d88ca44760a34be0b4d5eb13f6567a476
Matt Wrock <matt@mattwrock.com>
2016-06-01 19:03:52 -0700

    adding ci tasks that will install the correct versioni of bundler

--- ci/version_bump.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-gem update --system $(grep rubygems omnibus_overrides.rb | cut -d'"' -f2)
-gem install bundler -v $(grep bundler omnibus_overrides.rb | cut -d'"' -f2)
-bundle install
-
-bundle exec rake version:bump
