46159d136432f6bcd8abb7ca30f380ab195b0486
Matt Wrock <matt@mattwrock.com>
2016-06-01 18:41:07 -0700

    gem and bundler calls into version_bump

--- ci/version_bump.sh
+++ ci/version_bump.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-. ci/bundle_install.sh
+gem update --system $(grep rubygems omnibus_overrides.rb | cut -d'"' -f2)
+gem install bundler -v $(grep bundler omnibus_overrides.rb | cut -d'"' -f2)
+bundle install
 
 bundle exec rake version:bump
