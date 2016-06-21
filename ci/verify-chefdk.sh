29e84b7c4eddd352721ae98b473369629c87adaf
Matt Wrock <matt@mattwrock.com>
2016-06-21 10:55:44 -0700

    turning verify unit tests back on

--- ci/verify-chefdk.sh
+++ ci/verify-chefdk.sh
@@ -58,5 +58,5 @@ else
   # This has to be the last thing we run so that we return the correct exit code
   # to the Ci system. delivery-cli tests will cause a panic on some platforms
   # unless we set the terminal colors just right
-  sudo TERM=xterm-256color chef verify
+  sudo TERM=xterm-256color chef verify --unit
 fi
