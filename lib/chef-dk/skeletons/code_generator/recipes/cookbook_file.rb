0daa8b7361a22e7d0b90ad6a23f8cf1df2ecdd89
JJ Asghar <jj@chef.io>
2016-08-04 12:08:14 -0500

    No need for interpolation here
    Found a place that interpolation wasn't needed. Quick fix :metal:

--- lib/chef-dk/skeletons/code_generator/recipes/cookbook_file.rb
+++ lib/chef-dk/skeletons/code_generator/recipes/cookbook_file.rb
@@ -2,7 +2,7 @@
 context = ChefDK::Generator.context
 cookbook_dir = File.join(context.cookbook_root, context.cookbook_name)
 files_dir = File.join(cookbook_dir, "files", "default")
-cookbook_file_path = File.join(cookbook_dir, "files", "default", "#{context.new_file_basename}")
+cookbook_file_path = File.join(cookbook_dir, "files", "default", context.new_file_basename)
 
 directory files_dir do
   recursive true
