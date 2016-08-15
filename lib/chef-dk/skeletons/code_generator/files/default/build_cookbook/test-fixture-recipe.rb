b58a1f5f4d1c593a26e134f2902b90e719c07eb4
Jerry Aldrich III <jerry@chef.io>
2016-08-15 18:02:01 -0400

    Fix `Line is too long.` in `build_cookbook`

--- lib/chef-dk/skeletons/code_generator/files/default/build_cookbook/test-fixture-recipe.rb
+++ lib/chef-dk/skeletons/code_generator/files/default/build_cookbook/test-fixture-recipe.rb
@@ -1,6 +1,7 @@
 %w(unit lint syntax).each do |phase|
+  args = "--server localhost --ent test --org kitchen"
   # TODO: This works on Linux/Unix. Not Windows.
-  execute "HOME=/home/vagrant delivery job verify #{phase} --server localhost --ent test --org kitchen" do
+  execute "HOME=/home/vagrant delivery job verify #{phase} #{args}" do
     cwd '/tmp/repo-data'
     user 'vagrant'
   end
