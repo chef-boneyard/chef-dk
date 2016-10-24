c0a281dbfc8fb037bba39b4b219d22bcc23f9d07
JJ Asghar <jj@chef.io>
2016-10-24 14:18:32 -0700

    Added travis.yml and kitchen-docker.yml
    Having the ability to have travis.yml in the cookbook and
    kitchen-docker will allow for integration testing via travis.
    
    Signed-off-by: JJ Asghar  <jj@chef.io>

--- lib/chef-dk/skeletons/code_generator/recipes/cookbook.rb
+++ lib/chef-dk/skeletons/code_generator/recipes/cookbook.rb
@@ -128,6 +128,21 @@ if context.have_git
   end
 end
 
+# travis
+template "#{cookbook_dir}/.travis.yml" do
+  source 'travis.yml.erb'
+  helpers(ChefDK::Generator::TemplateHelper)
+  action :create_if_missing
+end
+
+# kitchen-docker.yml
+template "#{cookbook_dir}/.kitchen-docker.yml" do
+  source 'kitchen-docker.yml.erb'
+  helpers(ChefDK::Generator::TemplateHelper)
+  action :create_if_missing
+end
+
+
 if context.enable_delivery
 
   include_recipe "::build_cookbook"
