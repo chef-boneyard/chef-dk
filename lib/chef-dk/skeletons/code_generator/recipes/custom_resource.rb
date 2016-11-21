8747eccb2f561e045fed5892d51140d1db196df8
Franklin Webber <franklin.webber@gmail.com>
2016-11-21 10:13:25 -0600

    Adds 'chef generate resource RESOURCE_NAME'
    This command will generate a resources folder and a file.
    
    Signed-off-by: Franklin Webber <franklin@chef.io>

---
+++ lib/chef-dk/skeletons/code_generator/recipes/custom_resource.rb
@@ -0,0 +1,13 @@
+
+context = ChefDK::Generator.context
+cookbook_dir = File.join(context.cookbook_root, context.cookbook_name)
+
+resource_dir = File.join(cookbook_dir, "resources")
+resource_path = File.join(resource_dir, "#{context.new_file_basename}.rb")
+
+directory resource_dir
+
+template resource_path do
+  source "resource.rb.erb"
+  helpers(ChefDK::Generator::TemplateHelper)
+end
