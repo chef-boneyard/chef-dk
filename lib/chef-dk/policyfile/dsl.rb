0cce038a58707d45e2c4563c3feb6c19fe0cd2f7
Lamont Granquist <lamont@scriptkiddie.org>
2016-11-22 10:42:03 -0800

    use storage_config to get Dir.pwd properly
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- lib/chef-dk/policyfile/dsl.rb
+++ lib/chef-dk/policyfile/dsl.rb
@@ -138,7 +138,7 @@ module ChefDK
       end
 
       def metadata
-        cookbook_name = CookbookMetadata.from_path(".").cookbook_name
+        cookbook_name = CookbookMetadata.from_path(storage_config.relative_paths_root).cookbook_name
         name cookbook_name if name.nil?
         cookbook(cookbook_name, path: ".")
       end
