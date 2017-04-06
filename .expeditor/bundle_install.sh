2fd5b547c2316b5960f2742c4510f9d476d5fffe
Tom Duffield <tom@chef.io>
2017-04-06 12:10:45 -0700

    Add inline documentation to clarify Expeditor steps
    Signed-off-by: Tom Duffield <tom@chef.io>

--- .expeditor/bundle_install.sh
+++ .expeditor/bundle_install.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
+#
+# Simple file to perform a minimal bundle install to allow Chef Expeditor to run `rake` commands
+#
 
 set -evx
+
+# Only install groups required to run the Rake command
+export BUNDLE_WITHOUT=omnibus_package:test:aix:bsd:linux:mac_os_x:solaris:windows:default
 
 gem environment
 bundler_version=$(grep bundler omnibus_overrides.rb | cut -d'"' -f2)
 gem install bundler -v $bundler_version --user-install --conservative
-export BUNDLE_WITHOUT=omnibus_package:test:aix:bsd:linux:mac_os_x:solaris:windows:default
 bundle install
