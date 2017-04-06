d66bc2da4055815f9b316b92fd9bd3502f1d67e9
Tom Duffield <tom@chef.io>
2017-04-06 12:34:45 -0700

    Modify expeditor process via feedback from Lamont
    Signed-off-by: Tom Duffield <tom@chef.io>

--- .expeditor/update_version.sh
+++ .expeditor/update_version.sh
@@ -9,11 +9,21 @@ set -evx
 # The github-changelog-generator requires that LANG be set
 export LANG=en_US.UTF-8
 
+# Only install groups required to run the Rake command
+export BUNDLE_WITHOUT=omnibus_package:test:aix:bsd:linux:mac_os_x:solaris:windows:default
+
 # We need to run a bundle install so that our `bundle exec rake` command will work.
-. .expeditor/bundle_install.sh
+gem environment
+omnibus_bundler=$(grep bundler omnibus_overrides.rb | cut -d'"' -f2)
+gem install bundler -v $omnibus_bundler --user-install --conservative
+bundle install
 
 # Run a rake command that will update various files in chef/chef-dk with the new VERSION
-bundle exec rake expeditor_update_version
+bundle exec rake version:update_gemfile_lock
+
+# Run the following commands to update the changelog and dockerfile, but ignore errors.
+bundle exec rake changelog:update || true
+bundle exec rake update_dockerfile || true
 
 # Our `rake` command can sometimes modify this file, but we don't care about the
 # changes it makes. Reset it to HEAD.
