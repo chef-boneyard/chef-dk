94ad0c6cd55a795167b7db0ed26666c9c826971a
Tim Smith <tsmith@chef.io>
2017-08-31 13:47:34 -0700

    Revert all the way back to Rubygems 2.6.11 for more testing
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -1,7 +1,7 @@
 # THIS IS NOW HAND MANAGED, JUST EDIT THE THING
 # .travis.yml and appveyor.yml consume this,
 # try to keep it machine-parsable.
-override :rubygems, version: "2.6.13"
+override :rubygems, version: "2.6.11"
 override :bundler, version: "1.15.1"
 override "libffi", version: "3.2.1"
 override "libiconv", version: "1.15"
