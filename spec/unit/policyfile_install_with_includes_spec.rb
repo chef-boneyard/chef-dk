b8b40e8f27f105f00677512657233d36e528d74e
Jay Mundrawala <jay@thechamberofunderstanding.com>
2017-11-13 10:41:36 -0600

    Remove todo specs for other policyfile fetchers
    These tests are covered by making sure the dsl creates
    the correct type of fetcher, and the tests for those individual
    fetchers
    
    Signed-off-by: Jay Mundrawala <jay@thechamberofunderstanding.com>

--- spec/unit/policyfile_install_with_includes_spec.rb
+++ spec/unit/policyfile_install_with_includes_spec.rb
@@ -176,7 +176,7 @@ describe ChefDK::PolicyfileLock, "installing cookbooks from included policies" d
     end
   end
 
-  context "when a policy is included from local disk" do
+  context "when a policy is included" do
     let(:included_policy_lock_spec) do
       ChefDK::Policyfile::PolicyfileLocationSpecification.new(included_policy_lock_name, lock_source_options, nil).tap do |spec|
         allow(spec).to receive(:valid?).and_return(true)
@@ -199,7 +199,7 @@ describe ChefDK::PolicyfileLock, "installing cookbooks from included policies" d
       expect(policyfile.lock.cookbook_locks["cookbookC"].source_options).to eq(default_source_obj.source_options_for("cookbookC", "1.0.0"))
     end
 
-    it "maintains identifiers for remote cookbooks", :focus do
+    it "maintains identifiers for remote cookbooks" do
       allow(ChefDK::Policyfile::CachedCookbook).to receive(:new) do |name, storage_config|
         mock = ChefDK::Policyfile::CachedCookbook.allocate
         mock.send(:initialize, name, storage_config)
@@ -216,7 +216,6 @@ describe ChefDK::PolicyfileLock, "installing cookbooks from included policies" d
       expect(policyfile.lock.to_lock["cookbook_locks"]["cookbookB"]["source_options"]).to eq(included_policy_source_options["cookbookB"]["2.0.0"])
     end
 
-    # TODO: Sicne the PolicyfileLocationSpecification is a mock, this isn't really testing anything about a local lockfile
     it "emits the included policy in the lock file" do
       lock = policyfile.lock
       allow(lock).to receive(:cookbook_locks_for_lockfile).and_return({})
@@ -228,104 +227,6 @@ describe ChefDK::PolicyfileLock, "installing cookbooks from included policies" d
             "source_options" => lock_source_options,
           },
         ])
-    end
-
-    context "and the included policy sources cookbooks from local disk" do
-      # For example, imagine a directory layout like:
-      #
-      #    policies/
-      #      - policy_that_includes_shared_policy_a.rb
-      #      - shared_policy_folder/
-      #        - shared_policy_a.rb
-      #        - shared_policy_a.lock.json
-      #        - shared_policy_b.rb
-      #        - shared_policy_b.lock.json
-      #    cookbooks/
-      #      - webserver_cookbook/
-      #        <etc.>
-      #
-      # In `policies/shared_policy_folder/shared_policy_a.rb`, a cookbook may be
-      # sourced from local path with code like:
-      #
-      #     cookbook "webserver_cookbook", path: "../../cookbooks/webserver_cookbook"
-      #
-      # In the lockfile
-      # `policies/shared_policy_folder/shared_policy_a.lock.json`, we'd have some
-      # data like:
-      #
-      #     "cookbook_locks": {
-      #       "webserver_cookbook": {
-      #         "version": "0.1.0",
-      #         "identifier": "ea96c99da079db9ff3cb22601638fabd5df49599",
-      #         "source": "../../cookbooks/webserver_cookbook",
-      #
-      # However, when we want to `chef install` cookbooks for
-      # `policy_that_includes_shared_policy_a.rb`, we are one directory higher
-      # and the path needs to be adjusted to `../cookbooks/webserver_cookbook` (only one set of `..`s)
-      it "adjusts relative paths for local path cookbooks"
-
-      # counter-example to above, if the filesystem path is absolute, leave it as-is
-      it "does not adjust non-relative paths for local path cookbooks"
-
-      context "and the local path cookbook content is unchanged" do
-
-        it "does not raise an error when installing"
-
-      end
-
-      context "and the local path cookbook is modified so the identifier doesn't match" do
-
-        it "raises an error when installing"
-
-        it "suggests that the error may be resolved by updating the included policy"
-
-      end
-
-      context "and the local path cookbook does not exist" do
-
-        it "raises an error when installing"
-
-        it "includes the name and source location of the included policy"
-
-      end
-
-    end
-  end
-
-  context "when a policy is included from a Chef Server" do
-
-    it "maintains source locations for remote cookbooks (i.e., from github or supermarket)"
-
-    it "maintains identifiers for remote cookbooks"
-
-    context "and the included policy sources cookbooks from local disk" do
-
-      # NOTE: this will need to be a new kind of source in CookbookOmnifetch.
-      # We have support for "regular" cookbooks in Chef Server, but for this
-      # case we need to get the actual cookbook artifact at the specified revision_id
-      it "adjusts the source location for the cookbook to the Chef Server's cookbook artifact store"
-
-      it "maintains the identifier for the cookbooks"
-
-      context "and the cookbook_artifact has been removed from the chef server" do
-
-        # If the user is correctly using `chef clean-policy-revisions` and
-        # `chef clean-policy-cookbooks`, this case shouldn't occur, but should
-        # still have a clean and informative error
-        it "raises an error explaining that the included policy depends on a cookbook artifact that has been deleted"
-
-      end
-
-    end
-  end
-
-  context "when a policy is included from a remote git repo" do
-
-    context "and the included policy sources cookbooks from local disk" do
-
-      # ¯\_(ツ)_/¯
-      it "raises an error explaining that local path cookbooks cannot be used in policies included from git repos"
-
     end
   end
 end
