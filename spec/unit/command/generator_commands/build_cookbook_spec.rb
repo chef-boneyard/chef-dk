17399cdefda08f5f2adf38c3f9ea673383b86f74
Jennifer Davis <iennae@gmail.com>
2017-11-27 14:49:33 -0800

    At this point 12.14+ is the best option for the base cookbook generation command.
    Signed-off-by: Jennifer Davis <iennae@gmail.com>

--- spec/unit/command/generator_commands/build_cookbook_spec.rb
+++ spec/unit/command/generator_commands/build_cookbook_spec.rb
@@ -198,7 +198,7 @@ maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'all_rights'
 version '0.1.0'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 12.14' if respond_to?(:chef_version)
 
 depends 'delivery-truck'
 METADATA
