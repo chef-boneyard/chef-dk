8f4ab921640da76854eb0f7d1d2a4156d42d161a
Lamont Granquist <lamont@scriptkiddie.org>
2018-04-24 12:04:52 -0700

    remove integration group from inspec
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- omnibus/config/software/chef-dk.rb
+++ omnibus/config/software/chef-dk.rb
@@ -59,7 +59,7 @@ build do
   appbundle "chef", lockdir: project_dir, gem: "chef", without: %w{integration docgen maintenance ci travis}, env: env
   appbundle "foodcritic", lockdir: project_dir, gem: "foodcritic", without: %w{development}, env: env
   appbundle "test-kitchen", lockdir: project_dir, gem: "test-kitchen", without: %w{changelog debug docs provisioning}, env: env
-  appbundle "inspec", lockdir: project_dir, gem: "inspec", without: %w{deploy tools maintenance}, env: env
+  appbundle "inspec", lockdir: project_dir, gem: "inspec", without: %w{deploy tools maintenance integration}, env: env
 
   %w{chef-dk chef-vault ohai opscode-pushy-client cookstyle dco berkshelf}.each do |gem|
     appbundle gem, lockdir: project_dir, gem: gem, without: %w{changelog}, env: env
