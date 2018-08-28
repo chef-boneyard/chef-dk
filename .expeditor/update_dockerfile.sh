a2953bd7c6614a03fbdb0fb77db8ce288530ceb5
Tim Smith <tsmith@chef.io>
2018-08-28 09:25:49 -0700

    Add a sleep to the dep updater in Expeditor
    Attempt to wait long enough for rubygems to have the gem we need.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- .expeditor/update_dockerfile.sh
+++ .expeditor/update_dockerfile.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+############################################################################
+# What is this script?
+#
+# Chef-DK uses a workflow tool called Expeditor to manage version bumps, changelogs
+# and releases. When the current release of Chef-DK is promoted to stable this script
+# is run by Expeditor to update the version in the Dockerfile to match the stable
+# release.
+############################################################################
+
 set -evx
 
 sed -i -r "s/^ARG VERSION=.+/ARG VERSION=${VERSION}/" Dockerfile
