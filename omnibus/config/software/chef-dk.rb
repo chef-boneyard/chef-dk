b6c8d1c3438b9ca52230aa880fdcd4ae312e5a95
tyler-ball <tball@chef.io>
2018-11-02 11:51:03 -0600

    Preparing patch release with bug fixes
    Signed-off-by: tyler-ball <tball@chef.io>

--- omnibus/config/software/chef-dk.rb
+++ omnibus/config/software/chef-dk.rb
@@ -79,19 +79,10 @@ build do
     appbundle gem, lockdir: project_dir, gem: gem, without: %w{changelog}, env: env
   end
 
-  # Clear the now-unnecessary git caches, cached gems, and git-checked-out gems
-  block "Delete bundler git cache and git installs" do
+  # Clear git-checked-out gems (most of this cleanup has been moved into the chef-cleanup omnibus-software definition,
+  # but chef-client still needs git-checked-out gems)
+  block "Delete bundler git installs" do
     gemdir = shellout!("#{install_dir}/embedded/bin/gem environment gemdir", env: env).stdout.chomp
-    remove_directory "#{gemdir}/cache"
     remove_directory "#{gemdir}/bundler"
   end
-
-  # Clean up docs
-  delete "#{install_dir}/embedded/docs"
-  delete "#{install_dir}/embedded/share/man"
-  delete "#{install_dir}/embedded/share/doc"
-  delete "#{install_dir}/embedded/share/gtk-doc"
-  delete "#{install_dir}/embedded/ssl/man"
-  delete "#{install_dir}/embedded/man"
-  delete "#{install_dir}/embedded/info"
 end
