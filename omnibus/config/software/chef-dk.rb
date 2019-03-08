f8cfc06be73da93083fb93cd70e9db7a8eeb880a
Tim Smith <tsmith@chef.io>
2019-03-08 13:21:00 -0800

    Ignore a few more groups in appbundling
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- omnibus/config/software/chef-dk.rb
+++ omnibus/config/software/chef-dk.rb
@@ -58,8 +58,6 @@ build do
   env = with_standard_compiler_flags(with_embedded_path)
 
   excluded_groups = %w{server docgen maintenance pry travis integration ci}
-  excluded_groups << "ruby_prof" if aix?
-  excluded_groups << "ruby_shadow" if aix?
 
   # install the whole bundle first
   bundle "install --without #{excluded_groups.join(' ')}", env: env
@@ -70,9 +68,9 @@ build do
 
   env["NOKOGIRI_USE_SYSTEM_LIBRARIES"] = "true"
 
-  appbundle "chef", lockdir: project_dir, gem: "chef", without: %w{integration docgen maintenance ci travis}, env: env
+  appbundle "chef", lockdir: project_dir, gem: "chef", without: %w{integration docgen development maintenance travis}, env: env
   appbundle "foodcritic", lockdir: project_dir, gem: "foodcritic", without: %w{development}, env: env
-  appbundle "test-kitchen", lockdir: project_dir, gem: "test-kitchen", without: %w{changelog debug docs provisioning}, env: env
+  appbundle "test-kitchen", lockdir: project_dir, gem: "test-kitchen", without: %w{integration changelog debug docs chefstyle}, env: env
   appbundle "inspec", lockdir: project_dir, gem: "inspec", without: %w{deploy tools maintenance integration}, env: env
 
   %w{chef-dk chef-vault ohai opscode-pushy-client cookstyle dco berkshelf}.each do |gem|
