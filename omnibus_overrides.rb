26d449e6561ecd22f48bbddbbb37fcdfeacc5763
Tim Smith <tsmith@chef.io>
2019-04-12 16:00:09 -0700

    Use Ruby 2.5.5, Chef 14.11, and other newer deps
    A large bump of new releases. Most of these were made to allow pulling in the latest and greatest mixlib libraries so there's less here than actually appears at the surface.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -4,7 +4,7 @@
 #
 # NOTE: You MUST update omnibus-software when adding new versions of
 # software here: bundle exec rake dependencies:update_omnibus_gemfile_lock
-override :rubygems, version: "2.7.8"
+override :rubygems, version: "2.7.9"
 override :bundler, version: "1.17.3"
 override "libffi", version: "3.2.1"
 override "libiconv", version: "1.15"
@@ -16,7 +16,7 @@ override "libyaml", version: "0.1.7"
 override "makedepend", version: "1.0.5"
 override "ncurses", version: "5.9"
 override "pkg-config-lite", version: "0.28-1"
-override "ruby", version: "2.5.3"
+override "ruby", version: "2.5.5"
 override "ruby-windows-devkit-bash", version: "3.1.23-4-msys-1.0.18"
 override "util-macros", version: "1.19.0"
 override "xproto", version: "7.0.28"
