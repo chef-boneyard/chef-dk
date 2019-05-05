ef56c7b18a673327533fbb379200924d503604f3
Tim Smith <tsmith@chef.io>
2019-05-04 23:27:14 -0700

    Remove string literal comments from ruby code
    Rubocop doesn't recommend this now. The string literals can has been kicked down the road a bit further.
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/chef-dk/skeletons/code_generator/files/default/repo/cookbooks/example/attributes/default.rb
+++ lib/chef-dk/skeletons/code_generator/files/default/repo/cookbooks/example/attributes/default.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 # This is a Chef attributes file. It can be used to specify default and override
 # attributes to be applied to nodes that run this cookbook.
 
--- lib/chef-dk/skeletons/code_generator/files/default/repo/cookbooks/example/recipes/default.rb
+++ lib/chef-dk/skeletons/code_generator/files/default/repo/cookbooks/example/recipes/default.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 # This is a Chef recipe file. It can be used to specify resources which will
 # apply configuration to a server.
 
