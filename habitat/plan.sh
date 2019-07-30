f828462ed2ef43128c7c4b53f0ec2f37f2f597d4
Salim Afiune <afiune@chef.io>
2019-07-30 14:40:52 +0200

    restructure do_build to execute in a sub-shell
    As a best practice, we /shouldn't/ change the directory inside the
    callbacks since that position will be persistent throughout the build
    process of the package and latter other callbacks or habitat internals
    might conflict with that change of state, instead we should 1) execute
    commands that depend on a specific path inside a sub-sells or 2) use
    pushd and popd to move to a directory and get back where we were before.
    
    Number 1) is more desired but you could use them both.
    
    It is safer to first initialize the variables and then set the values,
    especially if you don't know what the content of the variable could be.
    
    All the bundle commands depend on running inside the cache path, I
    changed it to use sub-shells to run those commands.
    
    Signed-off-by: Salim Afiune <afiune@chef.io>

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -62,26 +62,34 @@ do_prepare() {
 }
 
 do_build() {
-  cd $CACHE_PATH
-  export CPPFLAGS="${CPPFLAGS} ${CFLAGS}"
+  local _bundler_dir
+  local _libxml2_dir
+  local _libxslt_dir
+  local _zlib_dir
+  export NOKOGIRI_CONFIG
+  export GEM_HOME
+  export GEM_PATH
 
-  local _bundler_dir=$(pkg_path_for bundler)
-  local _libxml2_dir=$(pkg_path_for libxml2)
-  local _libxslt_dir=$(pkg_path_for libxslt)
-  local _zlib_dir=$(pkg_path_for zlib)
+  _bundler_dir=$(pkg_path_for bundler)
+  _libxml2_dir=$(pkg_path_for libxml2)
+  _libxslt_dir=$(pkg_path_for libxslt)
+  _zlib_dir=$(pkg_path_for zlib)
 
-  export GEM_HOME=${pkg_prefix}
-  export GEM_PATH=${_bundler_dir}:${GEM_HOME}
+  NOKOGIRI_CONFIG="--use-system-libraries \
+    --with-zlib-dir=${_zlib_dir} \
+    --with-xslt-dir=${_libxslt_dir} \
+    --with-xml2-include=${_libxml2_dir}/include/libxml2 \
+    --with-xml2-lib=${_libxml2_dir}/lib \
+    --without-iconv"
+  GEM_HOME="$pkg_prefix"
+  GEM_PATH="${_bundler_dir}:${GEM_HOME}"
 
-  export NOKOGIRI_CONFIG="--use-system-libraries --with-zlib-dir=${_zlib_dir} --with-xslt-dir=${_libxslt_dir} --with-xml2-include=${_libxml2_dir}/include/libxml2 --with-xml2-lib=${_libxml2_dir}/lib --without-iconv"
-  bundle config --local build.nokogiri "${NOKOGIRI_CONFIG}"
-
-  bundle config --local silence_root_warning 1
-
-  bundle install --without dep_selector --no-deployment --jobs 2 --retry 5 --path $pkg_prefix
-
-  gem build chef-dk.gemspec
-  gem install --no-doc chef-dk-*.gem
+  ( cd "$CACHE_PATH" || exit_with "unable to enter hab-cache directory" 1
+    bundle config --local build.nokogiri "$NOKOGIRI_CONFIG"
+    bundle config --local silence_root_warning 1
+    bundle install --without dep_selector --no-deployment --jobs 2 --retry 5 --path "$pkg_prefix"
+    gem build ${pkg_name}.gemspec
+  )
 }
 
 do_install() {
