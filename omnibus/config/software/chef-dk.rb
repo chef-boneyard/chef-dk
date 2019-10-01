4a5ad8928d3214de6f75f6446342e67990074f3d
Tim Smith <tsmith84@gmail.com>
2019-10-01 14:15:43 -0700

    Skip the "test" group for inspec to prevent build failures
    passgen in this group was yanked. We no longer include passgen for
    testing in InSpec 4.x, but DK 3 is on InSpec 3.9.3, which has the dep
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- omnibus/config/software/chef-dk.rb
+++ omnibus/config/software/chef-dk.rb
@@ -75,7 +75,7 @@ build do
   appbundle "chef", lockdir: project_dir, gem: "chef", without: %w{integration docgen maintenance ci travis}, env: env
   appbundle "foodcritic", lockdir: project_dir, gem: "foodcritic", without: %w{development}, env: env
   appbundle "test-kitchen", lockdir: project_dir, gem: "test-kitchen", without: %w{changelog debug docs provisioning}, env: env
-  appbundle "inspec", lockdir: project_dir, gem: "inspec", without: %w{deploy tools maintenance integration}, env: env
+  appbundle "inspec", lockdir: project_dir, gem: "inspec", without: %w{deploy test maintenance integration}, env: env
 
   %w{chef-dk chef-apply chef-vault ohai opscode-pushy-client cookstyle dco berkshelf}.each do |gem|
     appbundle gem, lockdir: project_dir, gem: gem, without: %w{changelog}, env: env
