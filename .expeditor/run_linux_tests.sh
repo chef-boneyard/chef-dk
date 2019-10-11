5772cd50b53b1a19217074bfc13943c6de257e7a
Tim Smith <tsmith@chef.io>
2019-10-10 19:30:40 -0700

    Cache bundled gems in s3 to speed up testing (#2294)
    * Switch to the pre-warmed containers
    
    Signed-off-by: Tim Smith <tsmith@chef.io>
    
    * Experiment with saving the bundle to s3
    
    fingers crossed here
    
    Signed-off-by: Tim Smith <tsmith@chef.io>
    
    * Make sure we have aws tools
    
    Signed-off-by: Tim Smith <tsmith@chef.io>
    
    * Move everything into a helper script
    
    Signed-off-by: Tim Smith <tsmith@chef.io>
    
    * Use the full path to the script
    
    Signed-off-by: Tim Smith <tsmith@chef.io>
    
    * Compress the vendor bundle and upload just that file
    
    Signed-off-by: Tim Smith <tsmith@chef.io>
    
    * Only extract the archive if it exists
    
    Signed-off-by: Tim Smith <tsmith@chef.io>
    
    * I may not be the best at bash
    
    Signed-off-by: Tim Smith <tsmith@chef.io>
    
    * Just in case let's make sure to run in bash
    
    Signed-off-by: Tim Smith <tsmith@chef.io>
    
    * Use the standard containers again
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ .expeditor/run_linux_tests.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# This script runs a passed in command, but first setups up the bundler caching on the repo
+
+set -evx
+
+export USER="root"
+
+# make sure we have the aws cli
+apt-get update -y
+apt-get install awscli -y
+
+# grab the s3 bundler if it's there and use it for all operations in bundler
+echo "Fetching bundle cache archive from s3://public-cd-buildkite-cache/${BUILDKITE_PIPELINE_SLUG}/${BUILDKITE_LABEL}/bundle.tar.bz2"
+aws s3 cp "s3://public-cd-buildkite-cache/${BUILDKITE_PIPELINE_SLUG}/${BUILDKITE_LABEL}/bundle.tar.bz2" bundle.tar.bz2 || echo 'Could not pull the bundler directory to s3 for caching. Builds may be slower than usual as all gems will have to install.'
+
+echo "Restoring the bundle cache archive to vendor/bundle"
+if [ -f bundle.tar.bz2 ]; then
+  tar -xjf bundle.tar.bz2
+fi
+bundle config --local path vendor/bundle
+
+bundle install --jobs=7 --retry=3
+bundle exec $1
+
+# shove the current contents into s3 overwriting the existing bundle if anything changed
+
+echo "Creating the tar.bz2 to of the vendor/bundle directory to ship to s3"
+tar -cjf bundle.tar.bz2 vendor/
+echo "Uploading the tar.bz2 of the vendor/bundle directory to s3"
+aws s3 cp bundle.tar.bz2 "s3://public-cd-buildkite-cache/${BUILDKITE_PIPELINE_SLUG}/${BUILDKITE_LABEL}/bundle.tar.bz2" || echo 'Could not push the bundler directory to s3 for caching. Future builds may be slower if this continues.'
